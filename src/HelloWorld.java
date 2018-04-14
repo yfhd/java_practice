8045bc695f99bafdc8d9e96aeb16f3926ad4999f
yfhd <yefeizhao@163.com>
2018-04-14 21:09:40 +0200

    add hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        sayWords("Hallo Kokoti");
+    }
+	
+	public static void sayWords(String s) {
+		System.out.println(s);
+	}
+}
