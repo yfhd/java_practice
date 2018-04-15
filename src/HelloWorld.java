60b7b2197c568a30183d465601d2e64138e0e2bb
yfhd <yefeizhao@163.com>
2018-04-15 14:29:10 +0200

    Added new class FrsshJuice, in chapter basic-syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
 
+
+class FreshJuice {
+	enum FreshJuiceSize { SMAL, MEDIUM, LARGE }
+	
+	FreshJuiceSize size; 
+}
+
 public class HelloWorld {
 	public static void main(String[] args) {
         System.out.println("Hello World");
         
         sayWords("Hallo Kokoti");
+        
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
     }
 	
 	public static void sayWords(String s) {
