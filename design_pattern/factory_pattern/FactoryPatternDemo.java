ecec3b5e9c0efcef28c6664ce831596a792c6918
yfhd <yefeizhao@163.com>
2018-04-27 14:52:51 +0200

    added example for factory-pattern

---
+++ design_pattern/factory_pattern/FactoryPatternDemo.java
@@ -0,0 +1,18 @@
+package factory_pattern;
+
+public class FactoryPatternDemo {
+
+	public static void main(String[] args) {
+		ShapeFactory shapeFactory = new ShapeFactory();
+		
+		Shape shape1 = shapeFactory.getShape("CIRCLE");
+		shape1.draw();
+		
+		Shape shape2 = shapeFactory.getShape("RECTANGLE");
+		shape2.draw();
+		
+		Shape shape3 = shapeFactory.getShape("SQUARE");
+		shape3.draw();
+	}
+	
+}
---
+++ design_pattern/factory_pattern/Rectangle.java
@@ -0,0 +1,7 @@
+package factory_pattern;
+
+public class Rectangle implements Shape {
+    public void draw() {
+    	System.out.println("Inside Rectangle::draw() method.");
+    }
+}
